06b7d7a2711b74c956a231756e1ef454165cdbdd
AvetisDN <avetisdn@gmail.com>
2024-03-04 17:11:23 +0300

    links homepage

--- components/WhyChooseUsData.js
+++ components/WhyChooseUsData.js
@@ -3,21 +3,21 @@ export const WhyChooseUsData = [
     name: "Our Services",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/public/images/icon1.jpg",
-    link: "",
+    image: "/images/icon1.jpg",
+    link: "/day-tours",
   },
   {
     name: "Foreigner Tours",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/public/images/icon2.jpg",
-    link: "",
+    image: "/images/icon2.jpg",
+    link: "/destinations",
   },
   {
     name: "Car Rental",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/public/images/icon3.jpg",
-    link: "",
+    image: "/images/icon3.jpg",
+    link: "/car-rentals",
   },
 ];
